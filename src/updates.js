95c014ce978502c21217be3f216a79fab8979f6e
Bret Comnes <bcomnes@gmail.com>
2022-11-19 15:09:38 -0700

    Add update

--- src/updates.js
+++ src/updates.js
@@ -2,6 +2,13 @@ import { html } from 'uhtml-isomorphic'
 
 const data = [
   {
+    date: '2022-11-19',
+    content: html`Gumcast has migrated hosting companies which means that podcast feed subscriptions URLs will change.
+      Podcast apps should update automatically feeds from <code>gumcast-api.herokuapp.com</code> to <code>api.gumcast.com</code>, however sometimes this process fails due to bad podcast client behavior.
+      If you experience issues with your feed no longer updating, you may need to log into gumcast and generate a new feed URL, unsubscribe from your old subscription in your podcast app and re-add the new URL.
+      Apologies for the inconvenience. Old podcast feed URLs will work until November 28, 2022.`
+  },
+  {
     date: '2022-04-08',
     content: `Gumcast remains fully operational. 
       Since our December 2021 incident, Gumcast added extensive backend monitoring to more quickly detect and fix service interuptions.`
